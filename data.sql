edea8183661cfb413c94450c0c8aca7a434114c9
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-05-11 19:45:06 +0200

    changes into data

--- data.sql
+++ data.sql
@@ -87,6 +87,12 @@ CREATE TABLE `activity_images` (
 );
 
 
+INSERT INTO activity(name, description, image_1, image_2, image_3, image_4, nbpax, location) VALUES ('Jardin d asperges','Rendez-vous jeu avec des asperges','https://scx2.b-cdn.net/gfx/news/hires/fivebipedalm.jpg','http://www.ffrob.fr/img/Toulouse-Robot-Race.jpg','https://www.1jour1actu.com/wp-content/uploads/P5A9842vf.jpg','https://fracademic.com/pictures/frwiki/75/KSC-03PD-0786.jpg', 9, 'Martian Race');
+INSERT INTO activity(name, description, image_1, image_2, image_3, image_4, nbpax, location) VALUES ('Courses de Rovers','La Martian Robot Race est une course de vitesse : le vainqueur sera celui qui aura obtenu','https://scx2.b-cdn.net/gfx/news/hires/fivebipedalm.jpg','http://www.ffrob.fr/img/Toulouse-Robot-Race.jpg','https://www.1jour1actu.com/wp-content/uploads/P5A9842vf.jpg','https://fracademic.com/pictures/frwiki/75/KSC-03PD-0786.jpg', 9, 'Martian Race');
+INSERT INTO activity(name, description, image_1, image_2, image_3, image_4, nbpax, location) VALUES ('Le potager sur Mars','Apprenez à planter et récolter des asperges martiennes, activité à faire en famille !','https://cdn.futura-sciences.com/buildsv6/images/mediumoriginal/3/2/9/329fea1da1_102937_mars-potager-nasa.jpg','https://static.lexpress.fr/medias_11407/w_630,h_352,c_crop,x_0,y_6/w_2000,h_1125,c_fill,g_north/v1489140559/seul-sur-mars-nasa-patate-mars_5840711.jpg','https://img.over-blog-kiwi.com/0/55/36/22/20170322/ob_e8295c_w-jeb11438-copie-medium.jpg','https://recette.supertoinette.com/151347/m/asperges.jpg', 5, 'partout sur Mars');
+INSERT INTO activity(name, description, image_1, image_2, image_3, image_4, nbpax, location) VALUES ('Find the 7','Trouver les 7 rovers en marche et abandonnés disséminés sur la planète','https://upload.wikimedia.org/wikipedia/commons/a/a3/Rover1.jpg','https://www.numerama.com/wp-content/uploads/2021/01/curiosity-rover-mars.jpg','https://mars.nasa.gov/system/content_pages/main_images/382_viking-lander-PIA09703.jpg', 'https://www.cieletespace.fr/media/default/0001/15/Mars3_1000x700-5ac4.jpeg', 5, 'partout sur Mars');
+
+
 INSERT INTO circuit_images (url, is_main, id_circuit) values ('https://imgur.com/a/1uPSzAN', true, 1), ('', false, 1), ('https://imgur.com/a/W0m6W2P', true, 2), ('', false, 2);
 
 INSERT INTO activity_images (url, is_main, id_activity) values ('https://imgur.com/a/3boiSfg', true, 2), ('https://imgur.com/a/0nzQAhw', true, 1);
